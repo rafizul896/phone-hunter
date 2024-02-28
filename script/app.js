160ea6ad0356b9a90fcf86fab139baed5a1c0880
rafizul896 <rafizulislam896@gmail.com>
2024-02-28 15:11:00 +0600

    phone section added

--- script/app.js
+++ script/app.js
@@ -10,16 +10,17 @@ const displayPhones = phones => {
     phones.forEach(phones => {
         console.log(phones);
         const phoneItem = document.createElement('div');
-        phoneItem.classList = 'card w-96 bg-base-100 p-5 shadow-xl';
+        phoneItem.classList = 'card bg-base-100 p-5 border-2 shadow-xl mt-10 text-center';
         phoneItem.innerHTML = `
-        <figure>
+        <figure class = "bg-[#0D6EFD12] p-10 rounded-xl">
         <img src="${phones.image}" alt="Shoes" />
         </figure>
-        <div class="card-body">
-            <h2 class="card-title">${phones.phone_name}</h2>
-            <p>If a dog chews shoes whose shoes does he choose?</p>
-            <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+        <div class="card-body px-4 space-y-2">
+            <h2 class="text-xl font-bold text-center">${phones.phone_name}</h2>
+            <p class = "text-base text-[#706F6F]">${'There are many variations of passages of available, but the majority have suffered'}</p>
+            <h2 class="text-xl font-bold text-center">$999</h2>
+            <div class="card-actions justify-center">
+            <button class="border px-5 lg:px-7 py-2 bg-[#0D6EFD] text-white rounded-md lg:text-lg font-bold">Show Details</button>
             </div>
         </div>
         `;
